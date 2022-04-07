dbacb03474ad4c655a7dd3c2d398ee669e071811
Benjamin <rielaben@umich.edu>
2022-04-07 15:51:51 -0400

    validation code pt. 1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from openpyxl import load_workbook
 # %%
 # change quotes to fit the appropriate constellation and energystar files
 energystar_excel_file = "ESPM_Excel_input\AAPS_Real_Buildings_Template.xlsx"
-constellation_data_file = "Constellation_Excel_input\Ann Arbor Public Schools 3.31.22.xlsx"
+constellation_data_file = "Constellation_Excel_input\APPS_CONST_3.31.2022.xlsx"
 
 # %%
 def create_es_excel_wb(file_name):
